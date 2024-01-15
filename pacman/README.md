64a4b4815336e7418b83974d7688f90a6edf801f
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2024-01-15 00:30:08 -0800

    add ghosts, todo fix player bug

--- pacman/README.md
+++ pacman/README.md
@@ -4,3 +4,14 @@
 - Build a graph, with valid paths
 - Create ghosts that move on the graph
 - Create pacman that moves based on your input
+
+## Design
+- Gameboard exists
+- Player and ghosts exist on Gameboard
+- Each player and ghost has positions
+- player and ghosts are both just gaemobjs
+- diff in player and ghost is that ghost moves by itself
+- player moves by user input
+- player input is taken on separate thread
+- ghost AI is run on a separate thread
+- gameboard is redrawn on screen on an inerval -> N/sec, where N is frame rate
--- pacman/main.cpp
+++ pacman/main.cpp
@@ -1,10 +1,13 @@
 #include <cstdlib>
+#include <tuple>
+#include <random>
 #include <iostream>
 #include <memory>
 #include <sys/fcntl.h>
 #include <sys/termios.h>
 #include <termios.h>
 #include <sys/poll.h>
+#include <utility>
 #include <vector>
 #include <unistd.h>	// For usleep function
 #include <fcntl.h>	// for making stdin non-blocking
@@ -12,6 +15,7 @@
 #include <unordered_map>
 
 const char PACMAN = 'O';
+const char GHOST = 'X';
 const char QUIT = 'q';
 const char UP_CMD = 'w';
 const char DOWN_CMD = 's';
@@ -20,14 +24,44 @@ const char RIGHT_CMD = 'd';
 
 // Decides how many FPS to update
 const int FRAME = 100000;
-const int EVENT_LOOP_INPUT_POLL = 50;
+const int SECOND = 1000000;
+const int NON_BLOCKING_EVENT_LOOP_INPUT_POLL = 0;
+
+int getRandomInt(int upper) {
+    // Create a random device to obtain seed
+    std::random_device rd;
+
+    // Create a random number generator using the seed from the random device
+    std::mt19937 gen(rd());
+
+    // Define the distribution for integers in the range [0, upper]
+    std::uniform_int_distribution<int> distribution(0, upper);
+
+    // Generate a random integer
+    return distribution(gen);
+}
+
+void runCountdown(int i) {
+	system("clear");
+	std::cout << "Game starting in " << i << " seconds." << std::endl;
+	usleep(SECOND);
+	system("clear");
+	for (int j = i; j >= 0; j--) {
+		std::cout << j << " seconds to go!" << std::endl;
+		usleep(SECOND);
+		system("clear");
+	}
+}
 
 enum Direction {
 	UP,
 	DOWN,
 	RIGHT,
 	LEFT,
+	NOOP,
 };
+
+Direction dirFrom[4] = {UP, DOWN, LEFT, RIGHT};
 
 struct Input {
 	int moverId;
@@ -39,20 +73,36 @@ struct Position {
 	int y;
 };
 
+// forward decl
+class Ghost;
+
 class Gameboard {
 	public:
 		Gameboard(int rows, int cols);	
-		void draw();
+		void draw(std::vector<std::unique_ptr<Input> >& updates);
 		int insertMovable();
-		void updateMovable(const std::unique_ptr<Input>& input);
+
 	private:
 		std::vector<std::vector<char> > board;
 		int rows;
 		int cols;
 		int pidCounter;
 		std::unordered_map<int, std::unique_ptr<Position> > movables;
+		
+		std::pair<int, int> getCurrPos(int pid);
+
+		// update and get prev position
+		std::pair<int, int> updateMovable(const Input& input);
+
+		std::pair<std::pair<int, int>, Direction> validateMove(std::pair<int, int>& currPos, Direction dir);
+	
+		friend class Ghost;
 };
 
+std::pair<int, int> Gameboard::getCurrPos(int pid) {
+	const Position& pos = *this->movables[pid];
+	return std::make_pair(pos.y, pos.x);
+}
 
 Gameboard::Gameboard(int rows, int cols) : rows(rows), cols(cols), pidCounter(0) {
 	for (int i = 0; i < rows; i++) {
@@ -61,7 +111,7 @@ Gameboard::Gameboard(int rows, int cols) : rows(rows), cols(cols), pidCounter(0)
 	}
 }
 
-void Gameboard::draw() {
+void Gameboard::draw(std::vector<std::unique_ptr<Input> >& updates) {
 	for (int i = 0; i < this->rows; i++) {
 		for (int j = 0; j < this->cols; j++) {
 			std::cout << this->board[i][j] << " ";
@@ -69,53 +119,130 @@ void Gameboard::draw() {
 		std::cout << "\n";
 	}
 	
+	// go over the updates, make the updates and mark the inverse as empty
+	for (auto i = 0u; i < updates.size(); i++) {
+		const Input& update = *updates[i];
+		std::pair<int, int> prevPos = this->updateMovable(update);
+		// when someone has passed over the cell is now empty
+		this->board[prevPos.first][prevPos.second] = ' ';
+	}
+	
 	for (int i = 0; i < pidCounter; i++) {
 		const Position& pos = *this->movables[i];
-		this->board[pos.y][pos.x] = PACMAN;
+		std::cout << "moving PID: " << i << " TO " << pos.y << " , " << pos.x << std::endl;
+		char repr;
+		if (i == 0) {
+			repr = PACMAN;
+		} else {
+			repr = GHOST;
+		}
+		this->board[pos.y][pos.x] = repr;
 	}
 }
 
 int Gameboard::insertMovable() {
-	this->movables[this->pidCounter] = std::make_unique<Position>(Position{0, 0});
+	int row = 0;
+	int col = 0;
+
+	if (this->pidCounter != 0) {
+		row = getRandomInt(this->rows - 1);
+		col = getRandomInt(this->cols - 1);
+	}
+
+	this->movables[this->pidCounter] = std::make_unique<Position>(Position{row, col});
 	this->pidCounter++;
-	return this->pidCounter-1;
+
+	// the movable Id for the newly inserted movable
+	return this->pidCounter - 1;
 }
 
-void Gameboard::updateMovable(const std::unique_ptr<Input>& input) {
-	// refernce of movable pos, without taking ownership
-	Position& movablePos = *this->movables[input->moverId];
-	// TODO: bounds checking
-	switch (input->dir) {
+std::pair<std::pair<int, int>, Direction> Gameboard::validateMove(std::pair<int, int>& currPos, Direction dir) {
+	switch (dir) {
 		case UP:
-			if (movablePos.y > 0)
-				movablePos.y--;
+			if (currPos.first > 0)
+				return std::make_pair(std::make_pair(-1, 0), UP);
 			break;
 		case DOWN:
-			if (movablePos.y < rows-1)
-				movablePos.y++;
+			if (currPos.first < rows-1)
+				return std::make_pair(std::make_pair(1, 0), DOWN);
 			break;
 		case LEFT:
-			if (movablePos.x > 0)
-				movablePos.x--;
+			if (currPos.second > 0)
+				return std::make_pair(std::make_pair(0, -1), LEFT);
 			break;
 		case RIGHT:
-			if (movablePos.x < cols-1)
-				movablePos.x++;
+			if (currPos.second < cols-1)
+				return std::make_pair(std::make_pair(0, 1), RIGHT);
 			break;
 		default:
-			break;
+				break;
+	}
+	return std::make_pair(std::make_pair(0, 0), NOOP);
+}
+
+std::pair<int, int> Gameboard::updateMovable(const Input& input) {
+	// Take reference of movable pos, without taking ownership of it
+	Position& movablePos = *this->movables[input.moverId];
+	std::pair<int, int> initPos = std::make_pair(movablePos.y, movablePos.x);
+	
+	std::pair<int, int> offset = this->validateMove(initPos, input.dir).first;
+	movablePos.y += offset.first;
+	movablePos.x += offset.second;
+
+	if (movablePos.y == initPos.first && movablePos.x == initPos.second) {
+		return std::make_pair(-1, -1);
+	}
+
+	return initPos;
+}
+
+class Ghost {
+	public:
+		Ghost(int id);
+		std::unique_ptr<Input> getNextMove(Gameboard& gb);
+
+	private:
+		int id;
+		Direction lastMove;
+};
+
+// get random lastMove
+Ghost::Ghost(int id): id(id), lastMove(dirFrom[getRandomInt(3)]) {}
+
+std::unique_ptr<Input> Ghost::getNextMove(Gameboard& gb) {
+	while (true) {
+		Direction moveToMake;	
+		switch (this->lastMove) {
+			case NOOP:
+				moveToMake = DOWN;
+				break;
+			default:
+				moveToMake = lastMove;
+		}
+		// would making the move be a valid move on the board? if not give me a random diff dir
+		Input ghostInput;
+		ghostInput.moverId = this->id;
+		ghostInput.dir = moveToMake;
+	
+		std::pair<int, int> currPos = gb.getCurrPos(this->id);	
+		Direction dir = gb.validateMove(currPos, moveToMake).second;
+		if (dir != NOOP) {
+			this->lastMove = moveToMake;
+			return std::make_unique<Input>(std::move(ghostInput));		
+		}
+		this->lastMove = dirFrom[getRandomInt(3)];
 	}
 }
 
-// get updates from user and AI players using eventloop style IO multiplexing
+// get updates from user using eventloop style IO multiplexing
 int handleFakeInterrupt(struct pollfd fds[], std::vector<std::unique_ptr<Input> >& buf) {
 	// "1" specifies size of fds
-	int result = poll(fds, 1, EVENT_LOOP_INPUT_POLL);
+	int result = poll(fds, 1, NON_BLOCKING_EVENT_LOOP_INPUT_POLL);
 	// some FD has an event for us
 	if (result > 0) {
 		// based on which FD it is, we map to the player and move the player
 		if (fds[0].revents & POLLIN) {
-			char buffer[20];
+			char buffer[256];
 			ssize_t bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer));
 
 			if (bytesRead > 0) {
@@ -202,25 +329,37 @@ int main() {
 	std::vector<std::unique_ptr<Input> > gameplayInstructionBuffer;
 	
 	// setup gameboard
-	Gameboard gb(10, 10);
+	Gameboard gb(40, 40);
 	// add base player
-	gb.insertMovable();
+	int userId = gb.insertMovable();
+	int ghost1Id = gb.insertMovable();
+	int ghost2Id = gb.insertMovable();
+	std::cout << "User created with id: " << userId << std::endl;
+	std::cout << "2 ghosts added with ids " << ghost1Id << ", and " << ghost2Id << std::endl;
+
+	// add two ghosts
+	Ghost ghosts[2] = { 
+		Ghost(ghost1Id), 
+		Ghost(ghost2Id) 
+	};
+
+	runCountdown(3);
 
 	while (true)
 	{
+		for (int i = 0; i < 3; i++) {
+			gameplayInstructionBuffer.push_back(
+				ghosts[i].getNextMove(gb)
+			);
+		}
+
 		if (handleFakeInterrupt(fds, gameplayInstructionBuffer) < 0) {
 			// user wanted to exit the game
 			break;
 		}
 	
-		for (auto i = 0u; i < gameplayInstructionBuffer.size(); i++) {
-			std::unique_ptr<Input> instruction = std::move(gameplayInstructionBuffer[i]);
-			// make changes in each player
-			gb.updateMovable(instruction);
-		}
+		gb.draw(gameplayInstructionBuffer);
 		gameplayInstructionBuffer.clear();
-
-		gb.draw();
 
 		usleep(FRAME);
 
@@ -229,5 +368,6 @@ int main() {
 	}
 	
 	std::cout << "Thanks for playing!" << std::endl;
+	std::cout << "~ Hamdaan Khalid" << std::endl;
 	return 0;
 }
